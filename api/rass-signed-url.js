089b90cdbf470b27da9bbd3165cbbc664c967a76
QuentinPDesign <70437102+QuentinPDesign@users.noreply.github.com>
2026-04-10 11:06:07 +0200

    Update rass-signed-url.js

--- api/rass-signed-url.js
+++ api/rass-signed-url.js
@@ -11,7 +11,7 @@ const client = new S3Client({
 });
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://www.eastav.com");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   if (req.method === "OPTIONS") return res.status(200).end();
   const command = new GetObjectCommand({
